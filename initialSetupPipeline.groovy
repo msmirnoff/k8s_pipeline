f1ed71fc62a47e9919cecee52c930d4f93db51ac
Michael Smirnoff <michael@michaelsmirnoff.com>
2020-05-03 22:54:06 +0930

    Add NodeGroup creation to initial setup

--- initialSetupPipeline.groovy
+++ initialSetupPipeline.groovy
@@ -4,25 +4,15 @@ pipeline {
         stage('create the cluster') {
             environment {
                 STACK_NAME  = "kubeCluster"
-                CF_TEMPLATE = "cf_ekscluster.yml"
+                CF_TEMPLATE = "eks_creation.yaml"
             }
-            steps {
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-jenkins', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                    sh '''
-                        aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://"$CF_TEMPLATE" --capabilities CAPABILITY_NAMED_IAM 
-                    '''
-                }
-            }
-        }
-        stage('set the kubectl context') {
             steps {
                 withCredentials([
                     [$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-jenkins', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'],
-                    [$class: 'StringBinding', credentialsId: 'REGION_NAME', variable: 'REGION_NAME'],
-                    [$class: 'StringBinding', credentialsId: 'CLUSTER_NAME', variable: 'CLUSTER_NAME']])
+                    [$class: 'StringBinding', credentialsId: 'REGION_NAME', variable: 'REGION_NAME']])
                 {
                     sh '''
-                        aws eks --region "$REGION_NAME" update-kubeconfig --name "$CLUSTER_NAME"
+                        aws cloudformation --region $REGION_NAME create-stack --stack-name $STACK_NAME --template-body file://"$CF_TEMPLATE" --capabilities CAPABILITY_NAMED_IAM
                     '''
                 }
             }
